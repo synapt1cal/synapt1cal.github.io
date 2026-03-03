83933dde98091138c19d10dd43bed1b4b99f7eed
Synaptical <officialsynaptical@gmail.com>
2026-03-03 11:19:19 -0500

    Full website setup attempt 1

---
+++ css/writing/jjk-rewired/story.js
@@ -0,0 +1,15 @@
+// /writing/your-story/story.js
+const STORY_CONFIG = {
+  storyTitle:  'Jujutsu Kaisen: Rewired',
+  storyHref:   'index.html',
+  parts: [
+    {
+      label: 'Part 0',
+      title: 'The Prologue',
+      chapters: [
+        { num: 'Prologue', title: 'Silence before the Storm', href: 'prologue.html' },
+      ]
+    },
+    // add more parts here
+  ]
+};
